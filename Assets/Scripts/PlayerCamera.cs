c1b3f97b1621c01df1b90d392776b4cf0d79a0e5
Aiden <darkfoxthunder7@gmail.com>
2023-08-16 20:38:14 -0700

    Movement & Camera Orientation
    Added:
    -Camera rotates along with player orientation so movement is local
       -This makes the movement feel more natural since the player will always walk forward in the direction they are looking
    
    Changed:
    -Movement system to allow for the aforementioned orientation

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,24 +1,39 @@
+//Code copied & modified from Dave / GameDevelopment's channel 8/16/23
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public GameObject player;
-    Vector3 offset = new Vector3(0, 0, 0);
+    public float sensX;
+    public float sensY;
 
-    private float rotationX = 0.0f;
-    private float rotationY = 0.0f;
-    public float mouseSensitivity = 5.0f;
+    public Transform orientation;
 
-    // Update is called once per frame
-    void Update()
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        //positions camera to follow player with modifiable offset
-        transform.position = player.transform.position + offset;
-        //Makes camera move with mouse
-        rotationY += Input.GetAxis("Mouse X") * mouseSensitivity;
-        rotationX += Input.GetAxis("Mouse Y") * -1 * mouseSensitivity;
-        transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        //Locks and hides cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //gets mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //makes it so we can't look over 90 degrees in any direction
+
+
+        //rotate & orient camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,42 @@
+//Code copied from "Dave / GameDevelopment" because my code didn't have orientation 8/16/23
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
+    [Header("Movement")]
+    public float moveSpeed = 150.0f;
 
-    public float moveSpeed = 5.0f; // Adjust the move speed to your preference
+    public Transform orientation;
 
-    void Start()
+    private float horizontalInput;
+    private float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+    private void Start()
     {
-       // rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
-
-    void FixedUpdate()
+    private void Update()
     {
-
+        MyInput();
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }
