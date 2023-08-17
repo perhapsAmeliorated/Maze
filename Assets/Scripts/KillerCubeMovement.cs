bb67a40328f3d81975a4d4924140a571b4eeecd7
Aiden <darkfoxthunder7@gmail.com>
2023-08-16 22:06:30 -0700

    File Name Changes
    Changed:
    -Victor's "NewBehaviorScript" to "KillerCubeMovement" as cleanup

--- Assets/Scripts/KillerCubeMovement.cs
+++ Assets/Scripts/KillerCubeMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class KillerCubeMovement : MonoBehaviour
 {
     public GameObject player;
     public float killerSpeed = 3.3f;
