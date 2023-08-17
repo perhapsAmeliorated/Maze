c97a368899845b4305fae5066ce8d71a3ad5233f
prokiloer110 <142461826+prokiloer110@users.noreply.github.com>
2023-08-16 21:45:21 -0700

    killer cube
    + killer cube followers the player's movement at (4,6,0) offset
    + added real friction to player ("skin") and some bounciness (applied to some walls as well)

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject player;
+    public float killerSpeed = 3.3f;
+    public Vector3 offset = new Vector3(4, 6, 0);
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
