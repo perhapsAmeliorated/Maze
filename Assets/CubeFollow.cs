97f531bdaab8294b054b0d27d544d54abd4c4c81
prokiloer110 <142461826+prokiloer110@users.noreply.github.com>
2023-08-17 20:24:17 -0700

    friendly cube
    + added "com.unity.ai.navigation (ver 1.1.4)" package
    + baked navmesh for entire maze
    + killer cube now follows player around!

---
+++ Assets/CubeFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeFollow : MonoBehaviour
+{
+    public NavMeshAgent KillerCube;
+    public Transform player;
+
+    public float killerSpeed = 3.3f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KillerCube.SetDestination(player.position);
+    }
+}
