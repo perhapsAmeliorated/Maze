aad0eac6afe86c4dcbd10d98f3970be772e5fca1
prokiloer110 <142461826+prokiloer110@users.noreply.github.com>
2023-08-18 17:04:24 -0700

    cube sentience + menu
    + navmesh baked for maze
    + cube scripted to move
    - removed old cube code (useless!)
    + added menu plane with "play" textmesh (does nothing 💀)

--- Assets/Scripts/KillerCubeMovement.cs
+++ Assets/Scripts/KillerCubeMovement.cs
@@ -1,22 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class KillerCubeMovement : MonoBehaviour
 {
-    public GameObject player;
-    public float killerSpeed = 3.3f;
-    public Vector3 offset = new Vector3(4, 6, 0);
-    Rigidbody rb;
+    public NavMeshAgent killerCube;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        killerCube.SetDestination(player.position);
     }
 }
