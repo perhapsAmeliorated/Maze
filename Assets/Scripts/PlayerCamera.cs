875682a00408134de58f1935ec504c43203690e7
Aiden <darkfoxthunder7@gmail.com>
2023-08-22 17:02:00 -0700

    Player Camera Updates
    Changed:
    -Fov

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -27,7 +27,7 @@ public class PlayerCamera : MonoBehaviour
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
-
+        
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); //makes it so we can't look over 90 degrees in any direction
 
